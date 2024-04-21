12569a6cd85f6399f0984bd36a6c7f80be89502d
pavaris <pavaris.p@avlgb.com>
2024-04-21 23:46:12 +0700

    edit config db

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const mySqlPool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'haup_car'
+    database: process.env.DB_SCHEMA
 })
 
 module.exports = mySqlPool;
